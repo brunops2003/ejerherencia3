9546f57114f2b8792faa45dc1be8120a3c076aba
Bruno <brunoperezs2003@gmail.com>
2026-03-10 14:22:17 -0600

    herencia4

---
+++ clases/herencia1/auto_particular.py
@@ -0,0 +1,23 @@
+
+from clases.herencia2.persona import Persona
+
+class Autoparticular(Persona):
+    def __init__(self, clave, nombre, edad, marca ,color, placa):
+            super().__init__(clave, nombre, edad)
+
+            self.marca = marca
+            self.color = color
+            self.placa = placa 
+
+    def __str__(self):
+        return super().__str__()+""+self.marca+" "+self.color+" "+self.placa 
+
+    def subirseAuto(self):
+        print("Subiendo al auto")
+
+    def arrancarAuto(self):
+        print("Encendiendo el auto")
+        print("Arrancando el auto")
+
+    def llegarDestino(self):
+        print("Bajando el auto")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from clases.herencia1.taxi import Taxi 
+from clases.herencia1.auto_particular import Autoparticular
 def main(): 
     coche = Taxi("123-GTO","Versa",1000,"123-a") 
     print(coche) 
@@ -7,5 +8,13 @@ def main():
     coche.acelerar() 
     coche.frenar() 
     coche.cobrarCuota() 
+
+    ap = Autoparticular("123","Bruno",15,"volvo","plata","987-G")
+    print(ap)
+    ap.subirseAuto()
+    ap.arrancarAuto()
+    ap.llegarDestino()
+    
+
 if __name__ == '__main__': 
     main()
